0505366a6ea920f203cbbd9fb747ff1ddad2298a
Martín Farias <mfarias@rutatrol.com>
2018-08-01 13:18:35 -0300

    Enable to change words to bold, italic and underline

--- src/App.js
+++ src/App.js
@@ -3,24 +3,58 @@ import './App.css';
 import ControlPanel from "./control-panel/ControlPanel";
 import FileZone from "./file-zone/FileZone";
 import getMockText from './text.service';
-
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            file: ''
+            file: '',
+            selectedEl: null
         }
         this.getText = this.getText.bind(this)
+        this.textToBold = this.textToBold.bind(this)
+        this.textToItalic = this.textToItalic.bind(this)
+        this.textToUnderline = this.textToUnderline.bind(this)
+        this.wordAsSelected = this.wordAsSelected.bind(this)
     }
     componentDidMount(){
         this.getText()
     }
     getText() {
         getMockText().then((result) => {
+            var wordsMap = result.split(' ').map((word, index) => {
+                return <span key={index} onClick={this.wordAsSelected}> {word} </span>
+            })
             this.setState({
-                file: result
+                file: result,
+                fileEl: <div> {wordsMap} </div>
             });
         });
+    }
+    textToBold(){
+        var el = this.state.selectedEl;
+        el.style.fontWeight = !el.style.fontWeight ? 'bold' : '';
+        this.setState({
+            selectedEl: el
+        })
+    }    
+    textToItalic(){
+        var el = this.state.selectedEl;
+        el.style.fontStyle = !el.style.fontStyle ? 'italic' : '';
+        this.setState({
+            selectedEl: el
+        })
+    }
+    textToUnderline(){
+        var el = this.state.selectedEl;
+        el.style.textDecoration = !el.style.textDecoration ? 'underline' : '';
+        this.setState({
+            selectedEl: el
+        })
+    }
+    wordAsSelected(e){
+        this.setState({
+            selectedEl: e.target
+        })
     }
     render() {
         return (
@@ -29,8 +63,8 @@ class App extends Component {
                     <span>Advance Text Editor</span>
                 </header>
                 <main>
-                    <ControlPanel/>
-                    <FileZone file={this.state.file}/>
+                    <ControlPanel textToBold={this.textToBold} textToItalic={this.textToItalic} textToUnderline={this.textToUnderline}/>
+                    <FileZone file={this.state.fileEl}/>
                 </main>
             </div>
         );
--- src/control-panel/ControlPanel.js
+++ src/control-panel/ControlPanel.js
@@ -6,9 +6,9 @@ class ControlPanel extends Component {
         return (
             <div id="control-panel">
                 <div id="format-actions">
-                    <button className="format-action" type="button"><b>B</b></button>
-                    <button className="format-action" type="button"><i>I</i></button>
-                    <button className="format-action" type="button"><u>U</u></button>
+                    <button className="format-action" type="button" onClick={this.props.textToBold}><b>B</b></button>
+                    <button className="format-action" type="button" onClick={this.props.textToItalic}><i>I</i></button>
+                    <button className="format-action" type="button" onClick={this.props.textToUnderline}><u>U</u></button>
                 </div>
             </div>
         );
